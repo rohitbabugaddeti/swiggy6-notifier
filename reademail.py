06f6ec09c70980901667d5f97f02e7d699c3abb6
Rohit Babu <rohitbabu.gaddeti@gmail.com>
2019-04-12 15:29:40 +0530

    readmail ver2.0
    rouboust script

--- reademail.py
+++ reademail.py
@@ -22,12 +22,39 @@ for i in uids_list[::-1]:
     email_message=email.message_from_string(raw_email_string)
     #print(data[0])
     email_from=str(email.header.make_header(email.header.decode_header(email_message['From'])))
-    #email_subj=str(email.header.make_header(email.header.decode_header(email_message['Subject'])))
-    emails.add(email_from[email_from.index('<')+1:email_from.index('>')])
-    print(email_from[email_from.index('<')+1:email_from.index('>')])
-    #new_uid_list.append(i)
-    mail.uid('STORE',i,'+X-GM-LABELS','\\Trash')
-
+    email_subj=str(email.header.make_header(email.header.decode_header(email_message['Subject'])))
+    print('email subject: ',email_subj)
+    if 'remove' in email_subj.replace(' ','').lower():
+        s=shelve.open('test.db',writeback=True)
+        mails=s['emails']
+        print('before removal: ',mails)
+        e_id=email_from[email_from.index('<') + 1:email_from.index('>')]
+        try:
+            mails.remove(e_id)
+            s['emails'] = mails
+        except KeyError:
+            pass
+        #condition to check if remove requestd mail is already present in current read and removing it from current read emails set
+        if e_id in emails:
+            print('emails before remove',emails)
+            print('removed ',e_id ,' from current read')
+            emails.remove(e_id)
+        print('remove sucess')
+        print('after removal: ',s['emails'])
+        s.close()
+        mail.uid('STORE', i, '+X-GM-LABELS', '\\Trash')
+    else:
+        try:
+            emails.add(email_from[email_from.index('<') + 1:email_from.index('>')])
+            #print(email_from[email_from.index('<') + 1:email_from.index('>')])
+            #print(emails)
+            # new_uid_list.append(i)
+            mail.uid('STORE', i, '+X-GM-LABELS', '\\Trash')
+        except ValueError:
+            pass
+print('-----------------------------')
+print(emails)
+print('------------------------------')
 s=shelve.open('test.db',writeback=True)
 #s.clear()
 try:
